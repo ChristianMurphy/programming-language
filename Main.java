65d8d2e6fedcb6fd2d523b0945e5db06aef1d6e0
Christian Murphy <Christian.Murphy@asu.edu>
2015-04-13 12:06:23 -0700

    code can now be parsed

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -8,14 +8,14 @@ class Main {
     public static void main(String[] args) {
 		// Java read file stream from example.txt
 		try {
-			CharStream cs = (CharStream) new ANTLRFileStream("example.txt");
+			CharStream cs = new ANTLRFileStream("example.txt");
 			Team8Lexer lexer = new Team8Lexer(cs);
 			CommonTokenStream tokens = new CommonTokenStream(lexer);
 			Team8Parser parser = new Team8Parser(tokens);
 			ParserRuleContext tree = parser.root(); // parse
 
 			ParseTreeWalker walker = new ParseTreeWalker(); // create standard walker
-			Team8BaseListener extractor = new Team8BaseListener(parser);
+			Team8BaseListener extractor = new Team8BaseListener();
 			walker.walk(extractor, tree); // initiate walk of tree with listener
 		} catch (Exception e) {
 			e.printStackTrace();
